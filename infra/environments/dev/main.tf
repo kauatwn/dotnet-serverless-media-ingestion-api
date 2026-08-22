15706f746809076262d40e5f5e805bce6ce1ef2c
kauatwn <kaualopescontato@gmail.com>
2026-08-22 11:29:00 -0300

    refactor: update Lambda and API Gateway configurations with new stage name variable and remove unused VPC data

--- infra/environments/dev/main.tf
+++ infra/environments/dev/main.tf
@@ -10,18 +10,7 @@ locals {
   ecr_repo_name       = "${var.project_name}-lambda-${var.environment}"
   function_name       = "${var.project_name}-upload-image-${var.environment}"
   api_name            = "${var.project_name}-api-${var.environment}"
-}
-
-# Dynamic Network Resolution (Default VPC & Subnets)
-data "aws_vpc" "default" {
-  default = true
-}
-
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
+  bucket_arn          = "arn:aws:s3:::${local.bucket_name}"
 }
 
 # 1. Pure Module: Amazon S3 Bucket
@@ -37,18 +26,21 @@ module "s3" {
 module "dynamodb" {
   source = "../../modules/dynamodb"
 
-  dynamodb_table_name = local.dynamodb_table_name
-  environment         = var.environment
-  tags                = local.common_tags
+  dynamodb_table_name           = local.dynamodb_table_name
+  environment                   = var.environment
+  enable_point_in_time_recovery = false
+  tags                          = local.common_tags
 }
 
 # 3. Pure Module: Amazon ECR Repository
 module "ecr" {
   source = "../../modules/ecr"
 
-  ecr_repository_name = local.ecr_repo_name
-  environment         = var.environment
-  tags                = local.common_tags
+  ecr_repository_name  = local.ecr_repo_name
+  image_tag_mutability = "IMMUTABLE"
+  scan_on_push         = true
+  environment          = var.environment
+  tags                 = local.common_tags
 }
 
 # 4. Pure Module: AWS Lambda Function (Glue Code dynamically receiving outputs from ECR, S3, and DynamoDB)
@@ -62,9 +54,10 @@ module "lambda" {
   memory_size         = var.lambda_config.memory_size
   timeout             = var.lambda_config.timeout
   bucket_name         = module.s3.bucket_id
-  bucket_arn          = module.s3.bucket_arn
+  bucket_arn          = local.bucket_arn
   dynamodb_table_name = module.dynamodb.dynamodb_table_name
   dynamodb_table_arn  = module.dynamodb.dynamodb_table_arn
+  tracing_mode        = "PassThrough"
   environment         = var.environment
   tags                = local.common_tags
 }
@@ -74,9 +67,10 @@ module "apigateway" {
   source = "../../modules/apigateway"
 
   api_name             = local.api_name
-  stage_name           = "local"
+  stage_name           = var.stage_name
   lambda_invoke_arn    = module.lambda.lambda_invoke_arn
   lambda_function_name = module.lambda.lambda_function_name
+  binary_media_types   = []
   environment          = var.environment
   tags                 = local.common_tags
 }
