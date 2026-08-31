788f381d55f89fe60b756f93df46b4d57608dba0
kauatwn <kaualopescontato@gmail.com>
2026-08-31 19:15:39 -0300

    test: update DynamoDbRepositoryTests to use ImageMetadata.Create and include cancellation token in SaveMetadataAsync

--- tests/MediaIngestionApi.IntegrationTests/Infrastructure/Repositories/DynamoDbRepositoryTests.cs
+++ tests/MediaIngestionApi.IntegrationTests/Infrastructure/Repositories/DynamoDbRepositoryTests.cs
@@ -1,6 +1,6 @@
 using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.Model;
-using MediaIngestionApi.Core.Models;
+using MediaIngestionApi.Core.Entities;
 using MediaIngestionApi.Infrastructure.Repositories;
 using MediaIngestionApi.IntegrationTests.Abstractions;
 
@@ -28,16 +28,15 @@ public class DynamoDbRepositoryTests
     public async Task SaveMetadataAsync_ShouldPersistItem()
     {
         // Arrange
-        ImageMetadata metadata = new(
-            ImageId: Guid.NewGuid().ToString(),
-            FileName: "test-image.jpg",
-            SizeInBytes: 1024L,
-            S3Url: "https://s3.amazonaws.com/bucket/test-image.jpg",
-            UploadDate: DateTime.UtcNow
+        ImageMetadata metadata = ImageMetadata.Create(
+            fileName: "test-image.jpg",
+            sizeInBytes: 1024L,
+            s3Url: "https://s3.amazonaws.com/bucket/test-image.jpg",
+            timeProvider: TimeProvider.System
         );
 
         // Act
-        await _sut.SaveMetadataAsync(metadata);
+        await _sut.SaveMetadataAsync(metadata, TestContext.Current.CancellationToken);
 
         // Assert
         GetItemRequest request = new()
